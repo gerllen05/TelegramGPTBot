609244db9296c2f7d2fc9c195b182039e5ea31fa
Леонид Герловин <gerllen05@gmail.com>
2023-04-09 16:13:49 +0300

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -7,16 +7,16 @@ def str_to_bool(debug: str):
         return False
     
 MAIN_DB_URL = getenv("MYSQL_URL")
-POOL_SIZE = getenv("POOL_SIZE")
-POOL_TIMEOUT = getenv("POOL_TIMEOUT")
-POOL_RECYCLE = getenv("POOL_RECYCLE")
-MAX_OVERFLOW = getenv("MAX_OVERFLOW")
-ECHO = getenv("ECHO")
+POOL_SIZE = int(getenv("POOL_SIZE"))
+POOL_TIMEOUT = int(getenv("POOL_TIMEOUT"))
+POOL_RECYCLE = int(getenv("POOL_RECYCLE"))
+MAX_OVERFLOW = int(getenv("MAX_OVERFLOW"))
+ECHO = str_to_bool(getenv("ECHO"))
 
 DEBUG = str_to_bool(getenv("DEBUG"))
 
 TELEGRAM_API_KEY = getenv("TELEGRAM_API_KEY")
 
 GPT_API_KEY = getenv("GPT_API_KEY")
-CONTENT_AMOUNT = getenv("CONTENT_AMOUNT")
+CONTENT_AMOUNT = int(getenv("CONTENT_AMOUNT"))
 
